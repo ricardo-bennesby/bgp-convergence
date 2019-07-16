e92f5b14085ad7f32747802c0a2020a0401cfc68
ricardo-bennesby <ricardo.bennesby@gmail.com>
2019-07-16 13:29:57 -0400

    removed files

--- download_raw_updates/get_ripe_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-updates="updates.20181022*"
-
-for x in {00,01,03,04,05,06,07,10,11,13,14,16}; do  
-	mkdir rrc$x && cd rrc$x && pwd 
-	wget -A $updates -r -np -nc -l1 --no-check-certificate -e robots=off http://data.ris.ripe.net/rrc$x/2018.10/ &
-	cd ..
-done
